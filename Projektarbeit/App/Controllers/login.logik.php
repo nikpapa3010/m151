b5d340455b4f2a492ab41366c8248a4e50b730ec
Niko <60187035+nikpapa3010@users.noreply.github.com>
2021-01-18 10:56:24 +0100

    Fixed location for requirement

--- Projektarbeit/App/Controllers/login.logik.php
+++ Projektarbeit/App/Controllers/login.logik.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  include '../Core/usersrepository.inc.php';
+  include '../Repository/usersrepository.inc.php';
   
   // Falls Aufruf von Login-Seite
   if($_SERVER['REQUEST_METHOD'] === 'POST') 
