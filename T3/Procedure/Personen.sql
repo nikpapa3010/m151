4ec7dfc3ca430751d576344c65cc66ef086a5e4f
Niko <nikpapa3010@gmail.com>
2021-02-02 13:39:06 +0100

    Database triggers and prosejures

--- T3/Procedure/Personen.sql
+++ T3/Procedure/Personen.sql
@@ -1,4 +1,5 @@
-CREATE SCHEMA if not exists `m151` ;
+drop SCHEMA if  exists `m151` ;
+create schema `m151`;
 
 use m151;
 
@@ -10,5 +11,82 @@ CREATE TABLE if not exists `personen`
   `gehalt` double DEFAULT NULL,
   `geburtstag` date DEFAULT NULL,
   PRIMARY KEY (`personalnummer`)
-  );
+);
   
+  
+-- Insert or Update Customer --
+drop procedure if exists `Insert_UpsertCustomer`;
+DELIMITER $$ 
+CREATE  PROCEDURE  `Insert_UpsertCustomer`
+(
+	IN pNr int,
+    IN pName varchar(30),
+    IN pVorname varchar(25),
+    in pGehalt double,
+    in pGeburtstag date
+)
+BEGIN
+if  (exists(select personalnummer from personen where personalnummer = pNr)) then
+
+
+
+	update  `m151`.`personen`
+	set 		`name` = pName,
+				`vorname` = pVorname,
+				`gehalt` = pGehalt,
+				`geburtstag` = pGeburtstag
+			where `personalnummer` = pNr;
+
+else
+
+		insert into`m151`.`personen`
+				(`personalnummer`,
+				`name`,
+				`vorname`,
+				`gehalt`,
+				`geburtstag`)
+	VALUES	(pNr, pName, pVorname, pGehalt, pGeburtstag);
+end if;
+end $$
+
+call Insert_UpsertCustomer(1000, 'Muster', 'Max', 5500.0, '1990-05-01');
+-- call InsertCustomer(1000, 'Muster', 'Hans', 4000.0, '1990-05-01');
+call Insert_UpsertCustomer(200, 'Hans', 'Max', 500.0, '19500-09-05');
+select * from personen;
+
+
+-- Delete Person --
+drop procedure if exists `DeletePerson`;
+
+Delimitter $$
+create procedure `DeletePerson`( in pNr int)
+begin
+
+if (select Personalnummer from personen where personalnummer = pNr)
+	then delete from `m151`.`personen`
+			where `personalnummer` = pNr;
+
+	end if;
+end $$
+
+call DeletePerson(1000);
+select * from personen;
+
+
+-- Select Person By Id--
+drop procedure if exists `SelectPersonByID`;
+
+Delimitter $$
+create procedure `SelectPersonByID`(in pNr int)
+begin 
+
+ if (select Personalnummer from personen where personalnummer = pNr)
+	 then select * from `m151`.`personen`
+		 where `personalnummer` = pNr;
+
+ end if;
+end $$
+
+call SelectPersonByID(200);
+
+
---
+++ T3/Procedure/Personen_Triggers.sql
@@ -0,0 +1,8 @@
+-- 
+-- Trigger
+-- 
+
+use m151;
+
+alter table personen 
+	add column letzt_aenderung datetime null; 
