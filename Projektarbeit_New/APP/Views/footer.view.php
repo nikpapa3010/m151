1ca699df69cd78df2ac2e4d024fba013d83a15dc
Niko <60187035+nikpapa3010@users.noreply.github.com>
2021-02-03 14:31:01 +0100

    footer added

---
+++ Projektarbeit_New/APP/Views/footer.view.php
@@ -0,0 +1,45 @@
+<footer class="bg-dark text-lg-start text-light">
+<!-- Grid container -->
+<div class="container p-1">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h6 class="text-uppercase">Kontakt</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a  class="text-light">Adresszeile 1</a>
+          </li>
+          <li>
+            <a href="" class="text-light">Adresszeile 2</a>
+          </li>
+          <li>
+            <a href="" class="text-light">Adresszeile 3</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">e-mail@provider.ch</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="" class="text-light">Telefonnumer 1</a>
+          </li>
+          <br>
+          <li>
+            <a href="." class="text-light">Zum Kontaktformular</a>
+          </li>
+        </ul>
+      </div>
+
+      <!--Grid column-->
+
+        </ul>
+    </div>
+    </div>
+  
+
+
+</footer>
