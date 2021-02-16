ffe39ccd45d30e0db94c8d8966ca67be574a6abd
Niko <nikpapa3010@gmail.com>
2021-02-16 16:16:47 +0100

    Coments added

--- Projektarbeit_New/APP/database/Ski_service_User_Access.sql
+++ Projektarbeit_New/APP/database/Ski_service_User_Access.sql
@@ -1,12 +1,13 @@
 use ski_service;
 
-
+-- Give all Access to Admin --
 drop user if exists `SkiAdmin`@`%`;
 create user `SkiAdmin`@`%` identified by "SkiAdmin123$";
 grant all on ski_service.* to "SkiAdmin123$";
 
 
 
+-- Access to User --
 drop user if exists `SkiUser`@`%`;
 create user `SkiUser`@`%` identified by "SkiUser123$";
 grant select on ski_service.servicestatus to "SkiUser123$";
@@ -21,7 +22,7 @@ grant All on ski_service.benutzer to "SkiUser123$";
 grant All on ski_service.mietauftrag to "SkiUser123$";
 
 
-
+-- Access to Mitarbeiter -- 
 drop user if exists `SkiMitarbeiter`@`%`;
 create user `SkiMitarbeiter`@`%` identified by "SkiMitarbeiter123$";
 grant select on ski_service.servicestatus to "SkiMitareiter123$";
@@ -34,7 +35,3 @@ grant all on ski_service.serviceobjekt to "SkiMitareiter123$";
 grant all on ski_service.benutzer to "SkiMitareiter123$";
 grant all on ski_service.mietobjekt to "SkiMitareiter123$";
 grant all on ski_service.mietauftrag to "SkiMitareiter123$";
-
-
-
-
