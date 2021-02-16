48773698d52dcf5fd2d2b5e1fd2f14fd455c426e
Boggy <68187047+BOGGspotter@users.noreply.github.com>
2021-02-16 21:26:54 +0100

    SQL Benutzer Script korrigiert: identified by ist das Passwort, im Grant wird aber der Benutzername verlangt!

--- Projektarbeit_New/APP/database/Ski_service_User_Access.sql
+++ Projektarbeit_New/APP/database/Ski_service_User_Access.sql
@@ -3,35 +3,35 @@ use ski_service;
 -- Give all Access to Admin --
 drop user if exists `SkiAdmin`@`%`;
 create user `SkiAdmin`@`%` identified by "SkiAdmin123$";
-grant all on ski_service.* to "SkiAdmin123$";
+grant all on ski_service.* to `SkiAdmin`;
 
 
 
 -- Access to User --
 drop user if exists `SkiUser`@`%`;
 create user `SkiUser`@`%` identified by "SkiUser123$";
-grant select on ski_service.servicestatus to "SkiUser123$";
-grant select on ski_service.Serviceobjekt to "SkiUser123$";
-grant select on ski_service.Prioritaet to "SkiUser123$";
-grant select on ski_service.Rang to "SkiUser123$";
-grant select on ski_service.mietobjekttyp to "SkiUser123$";
-grant select on ski_service.mietobjekt to "SkiUser123$";
-grant select on ski_service.mietstatus to "SkiUser123$";
-grant All on ski_service.serviceauftrag to "SkiUser123$";
-grant All on ski_service.benutzer to "SkiUser123$";
-grant All on ski_service.mietauftrag to "SkiUser123$";
+grant select on ski_service.servicestatus to `SkiUser`;
+grant select on ski_service.Serviceobjekt to `SkiUser`;
+grant select on ski_service.Prioritaet to `SkiUser`;
+grant select on ski_service.Rang to `SkiUser`;
+grant select on ski_service.mietobjekttyp to `SkiUser`;
+grant select on ski_service.mietobjekt to `SkiUser`;
+grant select on ski_service.mietstatus to `SkiUser`;
+grant All on ski_service.serviceauftrag to `SkiUser`;
+grant All on ski_service.benutzer to `SkiUser`;
+grant All on ski_service.mietauftrag to `SkiUser`;
 
 
 -- Access to Mitarbeiter -- 
 drop user if exists `SkiMitarbeiter`@`%`;
 create user `SkiMitarbeiter`@`%` identified by "SkiMitarbeiter123$";
-grant select on ski_service.servicestatus to "SkiMitareiter123$";
-grant select on ski_service.prioritaet to "SkiMitareiter123$";
-grant select on ski_service.rang to "SkiMitareiter123$";
-grant select on ski_service.mietobjekttyp to "SkiMitareiter123$";
-grant select on ski_service.mietstatus to "SkiMitareiter123$";
-grant all on ski_service.serviceauftrag to "SkiMitareiter123$";
-grant all on ski_service.serviceobjekt to "SkiMitareiter123$";
-grant all on ski_service.benutzer to "SkiMitareiter123$";
-grant all on ski_service.mietobjekt to "SkiMitareiter123$";
-grant all on ski_service.mietauftrag to "SkiMitareiter123$";
+grant select on ski_service.servicestatus to `SkiMitarbeiter`;
+grant select on ski_service.prioritaet to `SkiMitarbeiter`;
+grant select on ski_service.rang to `SkiMitarbeiter`;
+grant select on ski_service.mietobjekttyp to `SkiMitarbeiter`;
+grant select on ski_service.mietstatus to `SkiMitarbeiter`;
+grant all on ski_service.serviceauftrag to `SkiMitarbeiter`;
+grant all on ski_service.serviceobjekt to `SkiMitarbeiter`;
+grant all on ski_service.benutzer to `SkiMitarbeiter`;
+grant all on ski_service.mietobjekt to `SkiMitarbeiter`;
+grant all on ski_service.mietauftrag to `SkiMitarbeiter`;
